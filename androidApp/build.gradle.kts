88b35f4751c60efe999c99ac7d8c52c73b34ea5c
JodliDev <jodlidev@gmail.com>
2022-10-04 14:36:36 +0200

    Version 2.6.1.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "at.jodlidev.esmira"
         minSdk = 21
         targetSdk = 31
-        versionCode = 121
-        versionName = "2.6.1.0"
+        versionCode = 122
+        versionName = "2.6.1.1"
         multiDexEnabled = true //project became too big. See: https://stackoverflow.com/a/59308589
     
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
