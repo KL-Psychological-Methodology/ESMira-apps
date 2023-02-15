b0db63aa7115d533e7e4249d9827812c1972e8af
JodliDev <jodlidev@gmail.com>
2023-02-15 13:34:48 +0100

    Fix crash of HTML views in iOS

--- iosApp/ESMira/Elements/HtmlTextView.swift
+++ iosApp/ESMira/Elements/HtmlTextView.swift
@@ -38,12 +38,7 @@ struct HtmlTextRepresentable: UIViewRepresentable {
 	}
 	
 	private func colorToHex(_ color: UIColor) -> String {
-		let components = color.cgColor.components
-		let r: CGFloat = components?[0] ?? 0.0
-		let g: CGFloat = components?[1] ?? 0.0
-		let b: CGFloat = components?[2] ?? 0.0
-
-		return String.init(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
+		return color.hexString
 	}
 	
 	private func updateHtml(_ label: UITextView) {
--- iosApp/ESMira/Workarounds.swift
+++ iosApp/ESMira/Workarounds.swift
@@ -17,3 +17,28 @@ extension List {
 		self.buttonStyle(PlainButtonStyle())
 	}
 }
+
+extension UIColor {
+	//Thanks to: https://stackoverflow.com/questions/26341008/how-to-convert-uicolor-to-hex-and-display-in-nslog
+	var hexString: String {
+		let cgColorInRGB = cgColor.converted(to: CGColorSpace(name: CGColorSpace.sRGB)!, intent: .defaultIntent, options: nil)!
+		let colorRef = cgColorInRGB.components
+		let r = colorRef?[0] ?? 0
+		let g = colorRef?[1] ?? 0
+		let b = ((colorRef?.count ?? 0) > 2 ? colorRef?[2] : g) ?? 0
+		let a = cgColor.alpha
+
+		var color = String(
+			format: "#%02lX%02lX%02lX",
+			lroundf(Float(r * 255)),
+			lroundf(Float(g * 255)),
+			lroundf(Float(b * 255))
+		)
+
+		if a < 1 {
+			color += String(format: "%02lX", lroundf(Float(a * 255)))
+		}
+
+		return color
+	}
+}
