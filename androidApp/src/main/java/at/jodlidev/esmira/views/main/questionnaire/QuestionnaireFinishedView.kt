1df852e169dc7f51ffae09b8190bab5c9a772cb8
JodliDev <jodlidev@gmail.com>
2023-05-03 10:59:34 +0200

    Increase padding in QuestionnaireFinishedView.kt

--- androidApp/src/main/java/at/jodlidev/esmira/views/main/questionnaire/QuestionnaireFinishedView.kt
+++ androidApp/src/main/java/at/jodlidev/esmira/views/main/questionnaire/QuestionnaireFinishedView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import at.jodlidev.esmira.ESMiraSurface
 import at.jodlidev.esmira.R
 import at.jodlidev.esmira.sharedCode.DbLogic
@@ -30,7 +32,7 @@ fun QuestionnaireFinishedView(
 			modifier = Modifier.fillMaxSize().clickable(onClick = close)
 		) {
 			Column(horizontalAlignment = Alignment.CenterHorizontally) {
-				Text(stringResource(R.string.info_questionnaire_success))
+				Text(stringResource(R.string.info_questionnaire_success), modifier = Modifier.padding(all = 20.dp))
 				TextButton(onClick = close) {
 					Text(stringResource(R.string.ok_))
 				}
