551ce30beeff0cd0bcaf7e7d88e2c6a00f98e4f8
JodliDev <jodlidev@gmail.com>
2023-06-29 14:16:00 +0200

    Android version 2.8.13

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "at.jodlidev.esmira"
         minSdk = 21
         targetSdk = 33
-        versionCode = 151
-        versionName = "2.8.12"
+        versionCode = 152
+        versionName = "2.8.13"
         multiDexEnabled = true //project became too big. See: https://stackoverflow.com/a/59308589
     
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
