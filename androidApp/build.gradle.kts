85bd631259eb0d995e090e833ebc89dbd342445e
Selina Volsa <57713712+SelinaDev@users.noreply.github.com>
2024-01-15 12:44:54 +0100

    Changed android version: 2.10.4

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "at.jodlidev.esmira"
         minSdk = 21
         targetSdk = 33
-        versionCode = 168
-        versionName = "2.10.3"
+        versionCode = 169
+        versionName = "2.10.4"
         multiDexEnabled = true //project became too big. See: https://stackoverflow.com/a/59308589
     
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
