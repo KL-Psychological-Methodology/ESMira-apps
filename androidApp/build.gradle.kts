3054a0e1080d5811c88ecf7cc2251691bb9bc252
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2024-05-02 14:32:11 +0200

    Update build.gradle.kts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "at.jodlidev.esmira"
         minSdk = 21
         targetSdk = 33
-        versionCode = 176
-        versionName = "2.13.0"
+        versionCode = 181
+        versionName = "2.13.2"
         multiDexEnabled = true //project became too big. See: https://stackoverflow.com/a/59308589
     
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
