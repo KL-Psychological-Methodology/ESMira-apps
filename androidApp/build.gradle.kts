1ee27a80325e6d58ec36ea62bb5d0ad2d6481450
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2024-12-10 17:16:15 +0100

    Update build.gradle.kts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "at.jodlidev.esmira"
         minSdk = 21
         targetSdk = 34
-        versionCode = 201
-        versionName = "2.15.1"
+        versionCode = 202
+        versionName = "2.15.2"
 
         multiDexEnabled = true //project became too big. See: https://stackoverflow.com/a/59308589
     
