f1f0aea926898cab918d3300daaa1bf267407277
SelinaDev <57713712+SelinaDev@users.noreply.github.com>
2025-10-13 13:15:25 +0200

    ensured language files are added to iOS

--- iosApp/ESMira/Elements/BatteryLevelView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BatteryLevelView.swift
-//  ESMira
-//
-//  Created by Karl Landsteiner Privatuniversität on 03.03.25.
-//  Copyright © 2025 orgName. All rights reserved.
-//
-
-import Foundation
