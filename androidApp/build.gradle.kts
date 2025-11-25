39155a46be8d409125f2e135a81bc95d0ba82812
Selina <57713712+SelinaDev@users.noreply.github.com>
2025-11-25 15:17:28 +0100

    actually fixed a missing check for skip toasts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "at.jodlidev.esmira"
         minSdk = 21
         targetSdk = 35
-        versionCode = 216
-        versionName = "2.17.6"
+        versionCode = 217
+        versionName = "2.17.7"
 
         multiDexEnabled = true //project became too big. See: https://stackoverflow.com/a/59308589
     
